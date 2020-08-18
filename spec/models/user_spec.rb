4b2f2164d1569dadde3a51dc501a5c0ec4a19d03
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-18 19:20:17 +0900

    編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,17 +29,17 @@ describe User do
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", 'First name is invalid')
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it 'family_nameが空では登録できない' do
         @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name can't be blank", 'Family name is invalid')
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana can't be blank", 'First name kana is invalid')
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
