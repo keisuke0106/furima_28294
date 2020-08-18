b9ac5a783c7c7e84e1e2ba5691dd37d0757ffa00
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-18 16:17:58 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
       validates :first_name
       validates :family_name
       validates :first_name_kana
       validates :family_name_kana
     end
-    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-      validates :password, format: { with: VALID_PASSWORD_REGEX }
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    birthday              {"1990-01-06"}
-    first_name            {"太郎"}
-    family_name           {"田中"}
-    first_name_kana       {"タロウ"}
-    family_name_kana      {"タナカ"}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    birthday              { '1990-01-06' }
+    first_name            { '太郎' }
+    family_name           { '田中' }
+    first_name_kana       { 'タロウ' }
+    family_name_kana      { 'タナカ' }
   end
 end
