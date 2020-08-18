5942ba0e9cfe0ba464a6a814cb870ef32261fce1
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-18 17:06:14 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
       validates :first_name
       validates :family_name
       validates :first_name_kana
-      validates :family_name_kana
-    end
+      validates :family_name_kana, format: {with: VALID_NAME_REGEX}
+
     VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX }
   end
