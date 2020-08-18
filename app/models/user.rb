d2c34c7fbbad62c86ec0effa34802a7f91092311
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-18 12:32:47 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :first_name
+      validates :family_name
+      validates :first_name_kana
+      validates :family_name_kana
+    end
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+      validates :password, format: { with: VALID_PASSWORD_REGEX }
+  end
 end
