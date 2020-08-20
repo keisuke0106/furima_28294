4d49230427c16b2b7ced050609fc98f60e293346
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-20 12:34:16 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-
-
+  belongs_to_active_hash :shipping_region
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :preparation_day
   belongs_to :user
   has_one :purchase_history
 
