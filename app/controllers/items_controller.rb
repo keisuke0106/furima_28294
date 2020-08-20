3073e18d081040dfe020d2c046bc71dabc08df40
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-20 13:38:32 +0900

    投稿機能、バリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,26 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @items = Item.all
-  end
-
-  def create
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @items = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_type_id, :shipping_region_id, :preparation_day_id, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,19 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage_type
   belongs_to_active_hash :preparation_day
 
-  validates :title, :text, :genre, presence: true
-  validates :genre_id, numericality: { other_than: 1 }
-
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category
+    validates :condition
+    validates :postage_type
+    validates :preparation_day
+    validates :shipping_region
+    validates :price
+  end
+  
+  validates :category_id, :condition_id, :postage_type_id, :shipping_region_id, :preparation_day_id, numericality: { other_than: 1 }
 
   belongs_to :user
   has_one :purchase_history
