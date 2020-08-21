5a2df79ca9277136ac52a17b1b030602ee9736d3
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-21 20:06:04 +0900

    出品機能

--- db/migrate/20200819041632_create_items.rb
+++ db/migrate/20200819041632_create_items.rb
@@ -1,14 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :image,              null: false
       t.string :name,               null: false
       t.text :explanation,          null: false
-      t.integer :category,          null: false
-      t.integer :condition,         null: false
-      t.integer :postage_type,      null: false
-      t.integer :shipping_region,   null: false
-      t.integer :preparation_day,   null: false
+      t.integer :category_id,          null: false
+      t.integer :condition_id,         null: false
+      t.integer :postage_type_id,      null: false
+      t.integer :shipping_region_id,   null: false
+      t.integer :preparation_day_id,   null: false
       t.integer :price,             null: false
       t.references :user,           null: false, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -34,14 +34,13 @@ ActiveRecord::Schema.define(version: 2020_08_20_050859) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
     t.string "name", null: false
     t.text "explanation", null: false
-    t.integer "category", null: false
-    t.integer "condition", null: false
-    t.integer "postage_type", null: false
-    t.integer "shipping_region", null: false
-    t.integer "preparation_day", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_type_id", null: false
+    t.integer "shipping_region_id", null: false
+    t.integer "preparation_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
