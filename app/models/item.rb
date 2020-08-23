0af62bf55954663bbc7215bd0628b3bba68b9dc0
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-23 12:32:03 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :postage_type_id
     validates :preparation_day_id
     validates :shipping_region_id
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   validates :category_id, :condition_id, :postage_type_id, :shipping_region_id, :preparation_day_id, numericality: { other_than: 1 }
