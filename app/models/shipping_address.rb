2c9371c9bbe2a86c380265d5f01720f90cdc013e
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-08-27 18:39:17 +0900

    購入画面に遷移

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,13 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase_history
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_name
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+
+  validates :shipping_region_id, numericality: { other_than: 1 }
 end
